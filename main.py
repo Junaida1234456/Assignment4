bbf5116227a4d3bde76ecb31a8bf9b3a08869911
Junaida1234456 <146915244+Junaida1234456@users.noreply.github.com>
2023-12-06 02:48:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 num1=float(input("enter no 1"))
 num2=float(input("enter no 2"))
+num3=float(input("enter no 3"))
 result= num1 + num2
+result1=result+num3
 print("SUM OF NUMBERS IS", result)
-print(result)
+print(result1)
